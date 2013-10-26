b3338006e0ca7db2468a0c86aa58320cdd8ff185
Scott Thompson <csthomp2@ncsu.edu>
2013-10-26 17:42:02 -0400

    Working timer; writes begin and end times to "times" table in database

--- js/index.js
+++ js/index.js
@@ -1,26 +1,65 @@
 document.addEventListener("deviceready", init, false);
 
 function init() {
-	alert('hello');
 	clockRun();
 }
 
 function clockRun() {
+	var interval;
+	
+	var db = window.openDatabase("time-card", "1.0", "Time Card Database", 1000000);
+	db.transaction(initDB, errorDB, successDB);
+	
 	$('#clock').on('pagebeforeshow', function() {
 		
 	});
 	$('#start').on('tap', function() {
 		var time = new Date();
-		time_print = time.toTimeString();
-		window.localStorage.setItem('start_time',time.getTime());
-		//$('#elapsed_time').html(time_print);
+		start_time = time.getTime();
+		db.transaction(function(tx) {
+			tx.executeSql('INSERT INTO times (start) VALUES ("' + start_time + '")', [], querySuccessDB, errorDB);
+		}, errorDB, successDB);
+		window.localStorage.setItem('start_time',start_time);
+		interval = window.setInterval(function() {
+			var time = new Date();
+			var end_time = time.getTime();
+			var time_elapsed = end_time-start_time;
+			$('#elapsed_time').html(elapsedTime(time_elapsed));
+		}, 1000);
 	});
 	$('#end').on('tap', function() {
-		var start_time = window.localStorage.getItem('start_time');
+		alert("END TIME: " + last_id);
 		var time = new Date();
 		var end_time = time.getTime();
-		var time_elapsed = end_time-start_time;
-		var print_time = time_elapsed.toTimeString();
-		$('#elapsed_time').html(print_time);
-	})
+		db.transaction(function(tx) {
+			tx.executeSql('UPDATE times SET end="' + end_time + '" WHERE id="' + last_id + '"');
+			tx.executeSql('UPDATE times SET description="temporary description" WHERE id="' + last_id + '"');
+		}, errorDB, successDB);
+		window.clearInterval(interval);
+	});
+}
+
+function initDB(tx) {
+	tx.executeSql('DROP TABLE IF EXISTS times');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS times (id INTEGER PRIMARY KEY AUTOINCREMENT, start, end, description)');
+}
+
+function errorDB(err) {
+	alert("SQL Error: " + err.code);
+}
+
+function successDB() {
+	
+}
+
+function querySuccessDB(tx, results) {
+	last_id = results.insertId;
+}
+
+function elapsedTime(time_elapsed) {
+	time_elapsed = time_elapsed/1000;
+	var hours = Math.floor(((time_elapsed % 31536000) % 86400) / 3600);
+	var minutes = Math.floor((((time_elapsed % 31536000) % 86400) % 3600) / 60);
+	var seconds = Math.floor((((time_elapsed % 31536000) % 86400) % 3600) % 60);
+	return hours + ":" + minutes + ":" + seconds;
 }
